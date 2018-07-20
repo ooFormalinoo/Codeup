36426bee9bdc65e37118edea5cfb770f58b170f0
ooFormalinoo <554531855@qq.com>
2018-07-20 19:42:48 +0800

    1940.cpp

--- 1940.cpp
+++ 1940.cpp
@@ -1,1 +1,18 @@
-#
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int a,b,c,d; 
+    for (n=1000;n<1112;n++){
+		a=n/1000;
+		b=n/100%10;
+		c=n/10%10;
+		d=n%10;
+		if (n*9==d*1000+c*100+b*10+a){
+			printf("%d\n",n);
+		} 
+		
+	}
+	return 0;
+
+}
